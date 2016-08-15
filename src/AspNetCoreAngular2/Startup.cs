17ff556b12da461bbeb99b15e8ea52e52e7d494a
Dmitry Slepchenko <slepchenko.dima@gmail.com>
2016-08-15 16:01:56 +0300

    update config.

--- src/AspNetCoreAngular2/Startup.cs
+++ src/AspNetCoreAngular2/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -34,7 +35,6 @@ namespace AspNetCoreAngular2
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-			loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddConsole();
 			
 			if (env.IsDevelopment())
@@ -46,18 +46,12 @@ namespace AspNetCoreAngular2
                 app.UseExceptionHandler("/Home/Error");
 			}
 
-			app.UseIISPlatformHandler();
             app.UseStaticFiles();
-			
-			app.UseMvc(routes =>
+
+            app.Run(async (context) =>
             {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-			});
+                await context.Response.WriteAsync("Hello world");
+            });
         }
-		
-		// Entry point for the application.
-		public static void Main(string[] args) => Microsoft.AspNet.Hosting.WebApplication.Run<Startup>(args);
     }
 }
