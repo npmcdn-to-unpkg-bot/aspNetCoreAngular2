777d677de212676d0a5506c065e1440d4537d33b
Dmitry Slepchenko <slepchenko.dima@gmail.com>
2016-08-15 15:29:38 +0300

    update configs

--- src/AspNetCoreAngular2/Startup.cs
+++ src/AspNetCoreAngular2/Startup.cs
@@ -12,29 +12,52 @@ namespace AspNetCoreAngular2
 {
     public class Startup
     {
+		public Startup(IHostingEnvironment env)
+        {
+            // Set up configuration sources.
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+		public IConfigurationRoot Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+			// Add framework services.
+			services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-
+			loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddConsole();
-
-            if (env.IsDevelopment())
+			
+			if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                app.UseExceptionHandler("/Home/Error");
+			}
+
+			app.UseIISPlatformHandler();
+            app.UseStaticFiles();
+			
+			app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+			});
         }
+		
+		// Entry point for the application.
+		public static void Main(string[] args) => Microsoft.AspNet.Hosting.WebApplication.Run<Startup>(args);
     }
 }
